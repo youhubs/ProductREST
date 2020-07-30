4867324fefcd3a844c52e79ebc0bc9d24c50346a
Hu Tiger <huxy99@yahoo.com>
2020-07-30 15:44:05 -0500

    refactor Maven project

--- src/main/java/com/youhubs/Application.java
+++ src/main/java/com/youhubs/Application.java
@@ -1,13 +1,13 @@
-package com.youhubs.ProductREST;
+package com.youhubs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductRestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductRestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
