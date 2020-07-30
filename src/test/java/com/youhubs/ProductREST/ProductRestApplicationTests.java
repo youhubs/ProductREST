961da798936047da8735b02dbc28970c6309971d
Hu Tiger <huxy99@yahoo.com>
2020-07-30 15:40:24 -0500

    create Maven project

---
+++ src/test/java/com/youhubs/ProductREST/ProductRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youhubs.ProductREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
